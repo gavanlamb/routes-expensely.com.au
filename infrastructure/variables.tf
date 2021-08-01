447c44880dcda9e9d5f7392dc0ab44bf630810ff
gavan <gavan.jon.lamb@gmail.com>
2021-08-01 19:29:20 +1000

    Update tags

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -19,7 +19,8 @@ locals {
   
   default_tags = {
     Application = "Expensely"
-    Team = "Expensely"
+    Team = "Expensely Core"
     ManagedBy = "Terraform"
+    Environment = var.environment
   }
 }
